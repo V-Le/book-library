ced645ffc5b040f8858ed4d672197388d97b630a
V-Le-D <le.victor@hotmail.com>
2021-09-10 15:08:42 -0500

    fixed naming convention and refactored

--- scripts/script.js
+++ scripts/script.js
@@ -15,43 +15,57 @@ function Book(title, author, pages, read) {
 const bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'Not read');
 const bookTwo = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', '309 pages', 'Not read');
 
-//Creating inital table
-const tableOne = document.createElement('table');
-tableOne.style.border = '1px black solid';
-tableOne.classList.add('tableMain');
-body.appendChild(tableOne);
+//Creating initial table
+const tableMain = document.createElement('table');
+tableMain.style.border = '1px black solid';
+tableMain.classList.add('tableMain');
+body.appendChild(tableMain);
 
-const tableOneHead = document.createElement('thead');
-tableOne.appendChild(tableOneHead);
+const tableHeader = document.createElement('thead');
+tableMain.appendChild(tableHeader);
 
-const tableOneHeadTR = document.createElement('tr');
-tableOneHeadTR.innerHTML = `<th>Title</th>
-                            <th>Author</th>
-                            <th>Pages</th>
-                            <th>Read?</th>`
-tableOneHead.appendChild(tableOneHeadTR);
-const tableOneBody = document.createElement('tbody');
-tableOne.appendChild(tableOneBody);
+const tableHeaderRow = document.createElement('tr');
+tableHeaderRow.innerHTML = `<th>Title</th>
+                                <th>Author</th>
+                                <th>Pages</th>
+                                <th>Read?</th>`
+tableHeader.appendChild(tableHeaderRow);
+const tableBody = document.createElement('tbody');
+tableMain.appendChild(tableBody);
 
-function appendLibrary() { 
-  myLibrary.forEach(function(lib) {
-    const tableOneTR = document.createElement('tr');
-    tableOneTR.innerHTML = `<td><button class='deleteBtn'>-</button>${lib.title}</td>
-                            <td>${lib.author}</td>
-                            <td>${lib.pages}</td>
+function appendBookToLibrary() { 
+  myLibrary.forEach(function(book) {
+    const tableBodyRow = document.createElement('tr');
+    tableBodyRow.innerHTML = `<td><button class='deleteBtn'>-</button>${book.title}</td>
+                            <td>${book.author}</td>
+                            <td>${book.pages}</td>
                             <td><button class='readBtn'>&nbsp</button>
-                            <span class='readTog'>${lib.read}</span></td>`
-    tableOneTR.classList.add('tRow');
-    tableOneBody.appendChild(tableOneTR);
+                            <span class='readTog'>${book.read}</span></td>`
+    tableBody.appendChild(tableBodyRow);
   })
 }
 
+//Button to add books to library
+const btnAdd = document.querySelector('#ButtonAdd');
+btnAdd.addEventListener('click', function() {
+  let title = prompt('title', 'Alice\'s Adventures in Wonderland');
+  let author = prompt('author', 'Lewis Carroll');
+  let pages = prompt('pages', '70 pages');
+  let read = prompt('read', 'Not read');
+  addBookToLibrary(title, author, pages, read);
+});
+
+//Button to delete all books from library
+const btnDelete = document.querySelector('#ButtonDelete');
+btnDelete.addEventListener('click', deleteAllBooksFromLibrary);
+
+
 function addBookToLibrary(title, author, pages, read) {
   const addBook = new Book(title, author, pages, read);
-  createTable();
+  createLibraryTable();
 }
 
-function buttonReadToggle() {
+function btnReadToggle() {
     const btnToggle = document.querySelectorAll('.readBtn');
     const readText = document.querySelectorAll('.readTog');
     
@@ -66,43 +80,32 @@ function buttonReadToggle() {
     }
   }
 
-function buttonDelete() {
+function deleteBookFromLibrary() {
 const deleteBtn = document.querySelectorAll('.deleteBtn');
 
     for(let i=0; i <= deleteBtn.length-1; i++) {
     deleteBtn[i].addEventListener('click', function() {
     myLibrary.splice(i,1);
-    console.log(i);
-    createTable();
+    createLibraryTable();
     });
     }
 };
 
-function deleteAllBooks() {
+function deleteAllBooksFromLibrary() {
     if(myLibrary.length > 0) {
       myLibrary = [];
-      createTable();
+      createLibraryTable();
     }
   }
 
-function createTable() {
-  tableOneBody.innerHTML = '';
-  appendLibrary();
-  buttonReadToggle();
-  buttonDelete();
+function createLibraryTable() {
+  tableBody.innerHTML = '';
+  appendBookToLibrary();
+  btnReadToggle();
+  deleteBookFromLibrary();
 };
 
-const btnAdd = document.querySelector('#ButtonAdd');
-btnAdd.addEventListener('click', function() {
-  let title = prompt('title', 'Alice\'s Adventures in Wonderland');
-  let author = prompt('author', 'Lewis Carroll');
-  let pages = prompt('pages', '70 pages');
-  let read = prompt('read', 'Not read');
-  addBookToLibrary(title, author, pages, read);
-});
 
-const btnDelete = document.querySelector('#ButtonDelete');
-btnDelete.addEventListener('click', deleteAllBooks);
 
-createTable();
+createLibraryTable();
 
